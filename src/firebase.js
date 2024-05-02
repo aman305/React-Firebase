d552aa5595a712193f750a2ac4f787245bc89967
amn305 <amanraj.ar206@gmail.com>
2024-05-02 23:43:10 +0530

    first

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDqta4iVccOltC2UIRFpuuScsEZTxUaFHA",
+  authDomain: "app-8ceb6.firebaseapp.com",
+  projectId: "app-8ceb6",
+  storageBucket: "app-8ceb6.appspot.com",
+  messagingSenderId: "365908457935",
+  appId: "1:365908457935:web:3143cbad31918ea944baba",
+  databaseURL: "https//app-8ceb6-default-rtdb.firebaseio.com",
+};
+
+export const app = initializeApp(firebaseConfig);
